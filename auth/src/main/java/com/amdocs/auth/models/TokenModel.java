7cb3aa91277af787e64b0764ad83d279764894b8
m-pulido <53849625+m-pulido@users.noreply.github.com>
2023-06-21 13:18:05 -0600

    adding TokenModel

---
+++ auth/src/main/java/com/amdocs/auth/models/TokenModel.java
@@ -0,0 +1,44 @@
+package com.amdocs.auth.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="token")
+public class TokenModel {
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	Long id;
+	
+	Long userId;
+	
+	String token;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+}
