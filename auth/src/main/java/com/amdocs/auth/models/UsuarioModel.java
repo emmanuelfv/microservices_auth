a57a24e4b14f8abdb47f6eb993c408a93cace40e
Jose Fernandez <josejavf@amdocs.com>
2023-06-21 13:27:38 -0600

    added imports to UsuarioModel

--- auth/src/main/java/com/amdocs/auth/models/UsuarioModel.java
+++ auth/src/main/java/com/amdocs/auth/models/UsuarioModel.java
@@ -1,3 +1,13 @@
+package com.amdocs.auth.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import jakarta.persistence.Id;
+
 @Entity
 @Table(name="usuario")
 public class UsuarioModel {
